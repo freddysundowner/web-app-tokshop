15de5f0bc4bace9daa3361ba43341868be76f36f
freddysundowner <githumbi3fred@gmail.com>
2025-11-16 22:26:02 +0300

    al

--- packages/admin-for-flutter/shared-backend/server/types/express-session.d.ts
+++
@@ -1,8 +0,0 @@
-import 'express-session';
-
-declare module 'express-session' {
-  interface SessionData {
-    user?: any; // User data from Tokshop API
-    accessToken?: string; // JWT token from Tokshop API
-  }
-}
